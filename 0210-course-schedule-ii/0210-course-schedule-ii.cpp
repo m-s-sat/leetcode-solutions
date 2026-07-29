cb85c7572f9addb797a6a0bc5ff07f606eb06699
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-07-29 11:23:28 +0530

    Time: 8 ms (21.85%), Space: 18.3 MB (70.94%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            // 1-->0 graph structure
+            int first = prerequisites[i][0];
+            int second = prerequisites[i][1];
+            adj[second].push_back(first);
+        }
+        int n = adj.size();
+        queue<int> q;
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()!=numCourses) return {};
+        return topo;
+    }
+};
