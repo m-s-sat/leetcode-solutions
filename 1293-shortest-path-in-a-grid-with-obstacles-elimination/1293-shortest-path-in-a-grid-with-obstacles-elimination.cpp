d4e785238e79c5fb44f61d4dbc8a47ae0a39ffdf
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-09 19:58:34 +0530

    Time: 0 ms (100%), Space: 12.5 MB (95.56%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -8,12 +8,10 @@ public:
         > pq;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dist(n, vector<int>(m,1e9));
         vector<vector<int>> minBricks(n, vector<int>(m, 1e9));
         int brick = grid[0][0]==0?0:1;
         minBricks[0][0] = brick;
         pq.push({0,{{0,0},brick}});
-        dist[0][0] = 0;
         int drow[4] = {-1,0,1,0};
         int dcol[4] = {0,1,0,-1};
         while(!pq.empty()){
