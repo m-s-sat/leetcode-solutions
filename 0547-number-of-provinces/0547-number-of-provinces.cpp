9dc4721b8b5bffb188527647a425728bd2cfdd79
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-12 15:13:47 +0530

    Time: 3 ms (36.2%), Space: 19.4 MB (82.75%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,49 @@
+class DisjointSet {
+public:
+    vector<int> parent, size;
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUlParent(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = findUlParent(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int ulp_u = findUlParent(u);
+        int ulp_v = findUlParent(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]>size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    ds.unionBySize(i,j);
+                };
+            }
+        }
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i]==i) cnt++;
+        }
+        return cnt;
+    }
+};
