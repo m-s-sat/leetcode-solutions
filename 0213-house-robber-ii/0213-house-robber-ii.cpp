91497af8d183ff725b4a4446f22d4f9a343e70bd
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-23 16:20:01 +0530

    Time: 0 ms (100%), Space: 10 MB (97.33%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,5 +1,29 @@
 class Solution {
 private:
+    int spaceOptimization(vector<int>& nums, int n){
+        int prev1 = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=prev2;
+            int not_take = prev1;
+            int curr = max(take, not_take);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    int tabulation(vector<int>& nums, int n){
+        vector<int> dp(n,-1);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int not_take = dp[i-1];
+            dp[i] = max(take, not_take);
+        }
+        return dp[n-1];
+    }
     int f(vector<int>& nums, int ind,vector<int>& dp){
         if(ind==0) return nums[ind];
         if(ind<1) return 0;
@@ -12,14 +36,10 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        vector<int> dp(n-1,-1);
-        int num1 = f(nums, n-2,dp);
+        int num1 = spaceOptimization(nums, n-1);
         vector<int> arr;
-        for(int i=1;i<n;i++){
-            arr.push_back(nums[i]);
-            dp[i-1] = -1;
-        }
-        int num2 = f(arr, n-2, dp);
+        for(int i=1;i<n;i++) arr.push_back(nums[i]);
+        int num2 = spaceOptimization(arr, n-1);
         return max(num1, num2);
     }
 };
