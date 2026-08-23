5efda55bbbdeef01aeb3a23599adbd60b9646f82
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-23 14:25:54 +0530

    Time: 3 ms (66.67%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftSum = 0, leftQ = 0;
+        int rightSum = 0, rightQ = 0;
+        for(int i=0;i<n/2;i++){
+            if(num[i]=='?') leftQ++;
+            else leftSum += num[i]-'0';
+        }
+        for(int i=n/2;i<n;i++){
+            if(num[i]=='?') rightQ++;
+            else rightSum += num[i]-'0';
+        }
+        if ((leftQ + rightQ) % 2) return true;
+        return leftSum-rightSum != (rightQ-leftQ)*9/2;
+    }
+};
