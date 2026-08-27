ba1f8fb06c4d42dd18bd126e757595330557ed0f
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-27 15:42:17 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,9 +7,29 @@ class Solution {
         int top = f(row-1,col,dp);
         return dp[row][col]=left+top;
     }
+    int f_tabulation(int n,int m){
+        int dp[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++) dp[i][j] = 0;
+        }
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(row==0&&col==0) dp[row][col] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if (row>0) up = dp[row - 1][col];
+                    if (col>0) left = dp[row][col - 1];
+                    dp[row][col]=up+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        // return f(m-1,n-1,dp);
+        return f_tabulation(m,n);
     }
 };
