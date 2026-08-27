14adcc07d1c7e8692ad09592e110c10e574bce7d
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-27 17:08:40 +0530

    Time: 0 ms (100%), Space: 15.5 MB (81.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -8,11 +8,32 @@ private:
         int left = grid[row][col]+f(grid,row,col-1,dp);
         return dp[row][col] = min(up,left);
     }
-public:
-    int minPathSum(vector<vector<int>>& grid) {
+    int f_tabulation(vector<vector<int>>& grid){
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(grid,n-1,m-1,dp);
+        int dp[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++) dp[i][j]=0;
+        }
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(row==0&&col==0) dp[row][col]=grid[row][col];
+                else{
+                    int up = 1e9, left=1e9;
+                    if(row>0) up = grid[row][col]+dp[row-1][col];
+                    if(col>0) left = grid[row][col]+dp[row][col-1];
+                    dp[row][col] = min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        // int n = grid.size();
+        // int m = grid[0].size();
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return f(grid,n-1,m-1,dp);
+        return f_tabulation(grid);
     }
 };
