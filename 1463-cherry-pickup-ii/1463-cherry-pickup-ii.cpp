fcd7e74cd989cb80bc61581da010d50d97daccd5
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-28 18:41:51 +0530

    Time: 51 ms (48.68%), Space: 13.8 MB (89.41%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -18,11 +18,49 @@ private:
         }
         return dp[i][j1][j2]=maxi;
     }
-public:
-    int cherryPickup(vector<vector<int>>& grid) {
+    int f_tabulation(vector<vector<int>>& grid){
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m, vector<int>(m, -1)));
-        return f(grid,0,0,m-1,n,m,dp);
+        int dp[n][m][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<m;k++) dp[i][j][k]=0;
+            }
+        }
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2) dp[n-1][j1][j2]=grid[n-1][j1];
+                else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int dcol[3] = {-1,0,1};
+                    int maxi = -1;
+                    for(int k=0;k<3;k++){
+                        for(int l=0;l<3;l++){
+                            int nj1=j1+dcol[k],nj2=j2+dcol[l];
+                            if(nj1<0||nj1>=m||nj2<0||nj2>=m) continue;
+                            int value;
+                            if(j1==j2) value = grid[i][j1];
+                            else value=grid[i][j1]+grid[i][j2];
+                            value+=dp[i+1][nj1][nj2];
+                            maxi=max(maxi,value);
+                        }
+                    }
+                    dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        // int n = grid.size();
+        // int m = grid[0].size();
+        // vector<vector<vector<int>>> dp(n,vector<vector<int>>(m, vector<int>(m, -1)));
+        // return f(grid,0,0,m-1,n,m,dp);
+        return f_tabulation(grid);
     }
 };
