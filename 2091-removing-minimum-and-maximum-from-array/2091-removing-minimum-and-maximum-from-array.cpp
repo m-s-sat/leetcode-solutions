5e7c2a357ed67b61c15ad16d801c27800ef9bfb0
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-08-30 06:25:27 +0530

    Time: 0 ms (100%), Space: 91.9 MB (98.33%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -3,10 +3,11 @@ public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return 1;
-        map<int,int> mp;
-        for(int i=0;i<n;i++) mp[nums[i]]=i;
-        auto [min_val, min_idx] = *mp.begin();
-        auto [max_val, max_idx] = *prev(mp.end());
+        int min_idx=0,max_idx=0;
+        for(int i=1;i<n;i++) {
+            if(nums[i]<nums[min_idx]) min_idx = i;
+            if(nums[i]>nums[max_idx]) max_idx = i;
+        }
         int f_case = max(min_idx,max_idx)+1;
         int s_case = n-min(min_idx,max_idx);
         int t_case = min(min_idx,max_idx)+n-max(min_idx,max_idx)+1;
