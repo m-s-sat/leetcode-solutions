e0ff28b508a97520d0429102f53231f568b018ae
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-09-06 15:53:04 +0530

    Time: 27 ms (67.19%), Space: 36.7 MB (50.06%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 private:
     int f(vector<int>& coins, int amount, int ind,vector<vector<int>>& dp){
-        if(amount==0) return 1;
         if(ind==0) return amount%coins[ind]==0;
         if(dp[ind][amount]!=-1) return dp[ind][amount];
         int not_take = f(coins,amount,ind-1,dp);
@@ -9,10 +8,47 @@ private:
         if(amount>=coins[ind]) take = f(coins,amount-coins[ind],ind,dp);
         return dp[ind][amount]=take+not_take;
     }
+    long long f_tabulation(vector<int>& coins, int amount){
+        int n = coins.size();
+        long long dp[n][amount+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=amount;j++) dp[i][j]=0;
+        }
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0) dp[0][j]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                long long not_take = dp[i-1][j];
+                long long take = 0;
+                if(j>=coins[i]) take = dp[i][j-coins[i]];
+                dp[i][j]=take+not_take;
+            }
+        }
+        return dp[n-1][amount];
+    }
+    long long space_optimisation(vector<int>& coins, int amount){
+        int n = coins.size();
+        vector<long long> prev(amount+1,0), curr(amount+1,0);
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0) prev[j]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                long long not_take = prev[j];
+                long long take = 0;
+                if(j>=coins[i]) take = curr[j-coins[i]];
+                curr[j]=take+not_take;
+            }
+            prev=curr;
+        }
+        return prev[amount];
+    }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n,vector<int>(amount+1,-1));
         return f(coins,amount,n-1,dp);
+        // return space_optimisation(coins,amount);
     }
 };
