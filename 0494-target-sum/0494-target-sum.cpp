44c055478383965ccea18ac4ed3526de173223d2
Mrinal Satyarthi <mrinalsatyarthi2005@gmail.com>
2026-09-06 15:13:08 +0530

    Time: 7 ms (67.33%), Space: 15.5 MB (46.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
-private:
-    int f(vector<int>& nums, int target, int ind,vector<vector<int>>& dp){
-        if(target<-1000 || target>1000) return 0;
+    int f(vector<int>& nums, int target, int ind, vector<vector<int>>& dp){
         if(ind==0){
             if(target==0 && nums[ind]==0) return 2;
-            if(target==nums[ind] || target==-nums[ind]) return 1;
+            if(target==0 || nums[ind]==target) return 1;
             return 0;
         }
-        if(dp[ind][target+1000]!=-1) return dp[ind][target+1000];
-        int sub = f(nums,target-nums[ind],ind-1,dp);
-        int plus = f(nums,target+nums[ind],ind-1,dp);
-        return dp[ind][target+1000]= plus+sub;
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int not_take = f(nums,target,ind-1,dp);
+        int take = 0;
+        if(target>=nums[ind]) take = f(nums,target-nums[ind],ind-1,dp);
+        return dp[ind][target]= take+not_take;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(20001,-1));
-        return f(nums,target,n-1,dp);
+        int sum = 0;
+        for(int i=0;i<n;i++) sum+=nums[i];
+        if((sum-target)%2!=0) return 0;
+        if(sum-target<0) return 0;
+        int targetSum = (sum-target)/2;
+        vector<vector<int>> dp(n,vector<int>(targetSum+1,-1));
+        return f(nums,targetSum,n-1,dp);
     }
 };
